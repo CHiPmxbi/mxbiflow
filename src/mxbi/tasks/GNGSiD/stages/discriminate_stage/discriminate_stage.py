8d74f8347227afeee6b65eaaa895f2ae1556c7a1
spcapitain <scapitain@dpz.eu>
2026-01-29 16:43:07 +0100

    Update discriminate_stage.py

--- src/mxbi/tasks/GNGSiD/stages/discriminate_stage/discriminate_stage.py
+++ src/mxbi/tasks/GNGSiD/stages/discriminate_stage/discriminate_stage.py
@@ -168,6 +168,6 @@ class GNGSiDDiscriminateStage:
         return self._stage_config.condition
 
     def _prepare_stimulus_intensity(self, monkey: str, frequency: int):
-        stimulus_intensity = choice([58,60,62])
+        stimulus_intensity = choice([60])
 
         return self._theater.acontroller.get_amp_value(frequency, stimulus_intensity)
