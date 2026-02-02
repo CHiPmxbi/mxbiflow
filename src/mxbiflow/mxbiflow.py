06ea7bebb0575f91033b0b6e921001072bb0f344
HuYang <huyangcommit@gmail.com>
2026-02-02 05:02:42 +0100

    feat: add MXBIFlow class with session and timer

---
+++ src/mxbiflow/mxbiflow.py
@@ -0,0 +1,39 @@
+from .models.session import Session
+from .timer.frame_timer import FrameTimer
+from pymxbi import MXBI
+
+
+class MXBIFlow:
+    def __init__(self, session: Session, mxbi: MXBI) -> None:
+        self._session = session
+        self._timer = FrameTimer()
+        self._mxbi = mxbi
+
+    @property
+    def session(self) -> Session:
+        return self._session
+
+    @property
+    def timer(self) -> FrameTimer:
+        return self._timer
+
+    @property
+    def mxbi(self) -> MXBI:
+        return self._mxbi
+
+
+_current_mxbiflow: MXBIFlow | None = None
+
+
+def get_mxbiflow() -> MXBIFlow:
+    global _current_mxbiflow
+    if _current_mxbiflow is None:
+        raise RuntimeError("MXBIFlow not initialized")
+    return _current_mxbiflow
+
+
+def set_mxbiflow(mxbiflow: MXBIFlow) -> None:
+    global _current_mxbiflow
+    if _current_mxbiflow is not None:
+        raise RuntimeError("MXBIFlow already initialized")
+    _current_mxbiflow = mxbiflow
