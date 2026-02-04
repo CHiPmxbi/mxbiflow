23c91ea9c15fabfab9723e52ce5c3b9bf6e42767
HuYang <huyangcommit@gmail.com>
2026-02-04 13:53:47 +0100

    feat: :sparkles: add serial utility functions for baudrates and ports

---
+++ src/mxbiflow/utils/serial.py
@@ -0,0 +1,10 @@
+from serial.tools import list_ports
+from serial import SerialBase
+
+
+def get_baudrates():
+    return [(str(baudrate), baudrate) for baudrate in SerialBase.BAUDRATES]
+
+
+def get_all_ports():
+    return [port.device for port in list_ports.comports() if port.device is not None]
