f942e209cde2c2be4872517c8ee75c60d3063b06
HuYang <huyangcommit@gmail.com>
2026-02-04 14:00:02 +0100

    refactor: update detector models and fix screen size typo

--- src/mxbiflow/models/mxbi.py
+++ src/mxbiflow/models/mxbi.py
@@ -4,17 +4,12 @@ from enum import StrEnum, auto
 from typing import Annotated, Union, TypeAlias, Literal
 
 from pydantic import BaseModel, Field
+from pymxbi.detector import DetectorType
 
 
 class RewarderTypeEnum(StrEnum):
     MOCK_REWARDER = auto()
     GPIO_REWARDER = auto()
-
-
-class DetectorTypeEnum(StrEnum):
-    MOCK_DETECTOR = auto()
-    RFID_DETECTOR = auto()
-    BEAM_RFID_DETECTOR = auto()
 
 
 class MXBIPlatformEnum(StrEnum):
@@ -57,14 +52,9 @@ RewarderModel: TypeAlias = Annotated[
 ]
 
 
-class BeamRFIDDetectorModel(BaseModel):
-    detector_type: Literal[DetectorTypeEnum.BEAM_RFID_DETECTOR] = (
-        DetectorTypeEnum.BEAM_RFID_DETECTOR
-    )
+class MockDetectorModel(BaseModel):
+    detector_type: Literal[DetectorType.MOCK] = DetectorType.MOCK
     detector_id: int = Field(default=0, ge=0)
-    beam_break_pin: int = Field(default=17, ge=0)
-    rfid_reader_port: str = Field(default="/dev/ttyUSB0")
-    rfid_baudrate: int = Field(default=9600, ge=1)
 
     enabled: bool = False
 
@@ -73,27 +63,46 @@ class BeamRFIDDetectorModel(BaseModel):
         return str(self.detector_type)
 
 
-class RFIDDetectorModel(BaseModel):
-    detector_type: Literal[DetectorTypeEnum.RFID_DETECTOR] = (
-        DetectorTypeEnum.RFID_DETECTOR
-    )
+class RFIDContinuousDetectorModel(BaseModel):
+    detector_type: Literal[DetectorType.RFID_CONTINUOUS] = DetectorType.RFID_CONTINUOUS
     detector_id: int = Field(default=0, ge=0)
+
+    enabled: bool = False
+
     port: str = Field(default="/dev/ttyUSB0")
     baudrate: int = Field(default=9600, ge=1)
-    enabled: bool = False
 
     @property
     def device_type(self) -> str:
         return str(self.detector_type)
 
 
-class MockDetectorModel(BaseModel):
-    detector_type: Literal[DetectorTypeEnum.MOCK_DETECTOR] = (
-        DetectorTypeEnum.MOCK_DETECTOR
+class BeamBreakContinuousDetectorModel(BaseModel):
+    detector_type: Literal[DetectorType.BEAMBREAK_CONTINUOUS] = (
+        DetectorType.BEAMBREAK_CONTINUOUS
     )
     detector_id: int = Field(default=0, ge=0)
 
     enabled: bool = False
+
+    pin: int = Field(default=17, ge=0)
+
+    @property
+    def device_type(self) -> str:
+        return str(self.detector_type)
+
+
+class FusionContinuousDetectorModel(BaseModel):
+    detector_type: Literal[DetectorType.FUSION_CONTINUOUS] = (
+        DetectorType.FUSION_CONTINUOUS
+    )
+    detector_id: int = Field(default=0, ge=0)
+
+    enabled: bool = False
+
+    pin: int = Field(default=17, ge=0)
+    port: str = Field(default="/dev/ttyUSB0")
+    baudrate: int = Field(default=9600, ge=1)
 
     @property
     def device_type(self) -> str:
@@ -101,7 +110,12 @@ class MockDetectorModel(BaseModel):
 
 
 DetectorModel: TypeAlias = Annotated[
-    Union[RFIDDetectorModel, BeamRFIDDetectorModel, MockDetectorModel],
+    Union[
+        MockDetectorModel,
+        RFIDContinuousDetectorModel,
+        BeamBreakContinuousDetectorModel,
+        FusionContinuousDetectorModel,
+    ],
     Field(discriminator="detector_type"),
 ]
 
@@ -109,6 +123,6 @@ DetectorModel: TypeAlias = Annotated[
 class MXBIModel(BaseModel):
     mxbi_id: int = Field(default=0, ge=0)
     platform: MXBIPlatformEnum = Field(default=MXBIPlatformEnum.RASPBIAN)
-    screen_szie: tuple[int, int] = Field(frozen=True)
+    screen_size: tuple[int, int] = Field(default=(1920, 1080))
     rewarders: list[RewarderModel] = Field(default_factory=list)
     detectors: list[DetectorModel] = Field(default_factory=list)
