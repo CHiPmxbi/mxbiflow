3db0883099cd7d06606df035fc63f00b830d12af
HuYang <huyangcommit@gmail.com>
2026-02-11 20:15:16 +0100

    refactor: :fire: remove unused stages field from Options

--- src/mxbiflow/models/session.py
+++ src/mxbiflow/models/session.py
@@ -151,4 +151,3 @@ class Options(BaseModel):
     mxbis: list[str] = Field(default_factory=list, frozen=True)
     experimenter: list[str] = Field(default_factory=list, frozen=True)
     animals: dict[str, str] = Field(default_factory=dict, frozen=True)
-    stages: list[str] = Field(default_factory=list)
