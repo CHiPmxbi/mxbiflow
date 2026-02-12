8f6ea51fb4e5770495aa80b3d89f15be7341811b
HuYang <huyangcommit@gmail.com>
2026-02-12 19:33:49 +0100

    feat: ✨ add center property to screen class

--- src/pymxbi/screen/screen.py
+++ src/pymxbi/screen/screen.py
@@ -5,3 +5,8 @@ from dataclasses import dataclass
 class Screen:
     width: int
     height: int
+
+    @property
+    def center(self) -> tuple[int, int]:
+        xshift = self.width * 0.25 * 0.75
+        return int(self.width * 0.5 + xshift), int(self.height * 0.5)
